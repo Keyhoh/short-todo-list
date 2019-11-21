33348802dea5c4dda55946891775be1c4218dd41
keyhoh <iheyhoh+github@gmail.com>
2019-11-21 16:55:39 +0900

    add length constraint

--- src/model/Todo/Todo.js
+++ src/model/Todo/Todo.js
@@ -4,6 +4,7 @@ export default class Todo{
     _title;
     constructor(title = 'title'){
         if(_.isEmpty(title)) throw 'Title cannot be empty';
+        if(title.length > 64) throw 'Title is too long';
         this._title = title ;
     }
 
--- test/TodoOperation.model.js
+++ test/TodoOperation.model.js
@@ -1,5 +1,6 @@
 import assert from 'assert';
 import Operation from "../src/model/Todo/Operation";
+import { isSafeGithubName } from 'app-builder-lib/out/platformPackager';
 
 describe('Todo operation test', function () {
     it('is default title', () => {
@@ -14,5 +15,12 @@ describe('Todo operation test', function () {
     it('occurs error by empty title', ()=>{
         assert.throws(() => Operation.create(''));
         assert.throws(() => Operation.create(null));
+    });
+
+    it('is too long title', () =>{
+        assert.doesNotThrow(() => Operation.create('01234567'.repeat(8)));
+        assert.throws(() => Operation.create('01234567'.repeat(8) + 'a'));
+        assert.doesNotThrow(() => Operation.create('０１２３４５６７'.repeat(8)));
+        assert.throws(() => Operation.create('０１２３４５６７'.repeat(8) + 'あ'));
     });
 });
