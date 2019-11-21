88ce2cd2fe35121a621f0d36dfdf913f96e08f46
keyhoh <iheyhoh+github@gmail.com>
2019-11-21 16:10:22 +0900

    add null title test

--- src/model/Todo/Operation.js
+++ src/model/Todo/Operation.js
@@ -3,6 +3,7 @@ import { isNull } from "util";
 
 export default class Operation{
     static create(title){
+        if(isNull(title)) throw 'Title cannot be null';
         return new Todo(title);
     }
 }
--- test/TodoOperation.model.js
+++ test/TodoOperation.model.js
@@ -9,4 +9,8 @@ describe('Todo operation test', function () {
     it('is able to set title', () => {
         return assert.equal(Operation.create('new title').title, 'new title');
     });
+
+    it('occurs error by null title', () => {
+        return assert.throws(() => Operation.create(null));
+    });
 });
