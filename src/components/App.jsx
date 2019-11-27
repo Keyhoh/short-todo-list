f1ba7882d36d90d00b124759b0f69e609f8dc2c0
keyhoh <iheyhoh+github@gmail.com>
2019-11-27 17:26:32 +0900

    AppコンポーネントのTODOを記載

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import List from "./List/ListComponent";
 
+/**
+ * state:
+ *      focusedList: フォーカスしているList（Listを特定できれば型は問わない）
+ */
 export default class App extends React.Component{
+    // TODO: StoreからTodoを取得する
+    // TODO: Storeから取得したTodoを状態で分類して、状態毎のListを作成する
+    // TODO: フォーカスしているListを強調する
     render(){
         return <List list={['A', 'B', 'C']}/>;
     }
