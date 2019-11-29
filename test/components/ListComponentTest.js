2d0ccf85519154611ce103a24d4c2e4175e6bdec
keyhoh <iheyhoh+github@gmail.com>
2019-11-29 18:18:53 +0900

    listがtodoを子に持つテストを実装

--- test/components/ListComponentTest.js
+++ test/components/ListComponentTest.js
@@ -1,16 +1,17 @@
-import { JSDOM } from 'jsdom';
 import path from 'path';
-import assert from 'assert';
+import chai, { expect } from 'chai';
+import chaiEnzyme from 'chai-enzyme';
+import Enzyme, { mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { act, Simulate } from 'react-dom/test-utils';
 import Store from "../../src/operation/Todo/Store";
 import List from "../../src/components/List/ListComponent";
+import Todo from "../../src/components/Todo/TodoComponent";
 
-/** @type {Window} */
-global.window = new JSDOM('<!DOCTYPE html><body></body>', { url: 'http://localhost' }).window;
-/** @type {Document} */
-global.document = window.document;
+Enzyme.configure({ adapter: new Adapter() });
+const debugLog = wrapper => wrapper.html();
+chai.use(chaiEnzyme(debugLog));
+
 global.App = { dataDir: path.resolve(__dirname, 'temp') };
 
 describe('List component test', function () {
@@ -19,19 +20,16 @@ describe('List component test', function () {
     const TODO_LIST = Store.findAll();
 
     this.beforeEach(() => {
-        container = document.createElement('div');
-        document.body.appendChild(container);
-        act(() => {
-            ReactDOM.render(<List list={TODO_LIST} />, container);
-        });
+        container = mount(<List list={TODO_LIST} />);
     });
 
     this.afterEach(() => {
-        document.body.removeChild(container);
         container = undefined;
     });
 
-    it('has the right amount of todo', () => {
-        assert.equal(container.getElementsByClassName('todo').length, TODO_LIST.length);
+    it('has the right todo', () => {
+        TODO_LIST.forEach(todo => {
+            expect(container).to.contain(<Todo key={todo.id} name={todo.title} />);
+        });
     });
 });
