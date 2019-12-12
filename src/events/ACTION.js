1583c2af1f5b6583da9ef8b76d8538183a89ab59
keyhoh <iheyhoh+github@gmail.com>
2019-12-12 11:08:17 +0900

    空のアクションを追加

---
+++ src/events/ACTION.js
@@ -0,0 +1,18 @@
+export const createTodo = () => { };
+export const checkTodo = () => { };
+export const discardTodo = () => { };
+export const pullUpTodo = () => { };
+export const deleteTodo = () => { };
+export const selectTodo = () => { };
+
+export const focusNextTodo = () => { };
+export const focusPrevTodo = () => { };
+export const gotoTop = () => { };
+export const gotoEnd = () => { };
+
+export const focusNextList = () => { };
+export const focusPrevList = () => { };
+
+export const switchMode = () => { };
+export const switchToNormalMode = () => { };
+export const switchToInsertMode = () => { };
